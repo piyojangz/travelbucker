3a7734737a5aac5a0fce3bb187f03513c86ff121
veerayuth <veerayuth@locus.co.th>
2018-02-21 15:07:52 +0700

    fix bugs

---
+++ ios/StarterKit-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  StarterKit-Bridging-Header.h
+//  StarterKit
+//
+//  Created by Breeshy Sama on 1/8/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#ifndef StarterKit_Bridging_Header_h
+#define StarterKit_Bridging_Header_h
+
+
+#endif /* StarterKit_Bridging_Header_h */
